855d70d1d4b4260a995c302dd0d5b1263f4f3aad
Rohit Turkar <rohittur2002@gmail.com>
2022-07-26 22:59:00 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,15 +1,40 @@
 const element=document.querySelector("#addExpense");
 
 let totalExpense=0;
-
+//get the heading Element
 const headingElement=document.querySelector("#heading");
+//ref to input Amount
+const inputElement=document.querySelector("#inputAmount");
+//get the reference to disc element
+const inputDescEl=document.querySelector("#inputDesc")
+//set the headingElement to totalExpense
 headingElement.textContent=totalExpense;
 
-element.addEventListener("click",()=>{
-const inputElement=document.querySelector("#inputAmount");
+// all expenses at once place
 
-let textValue=inputElement.value;
-let expense=parseInt(textValue,10);
+const allExpenses=[];
+
+element.addEventListener("click",()=>{
+
+    const expenseItem ={};
+
+//read the value from inputElement
+let textAmount=inputElement.value;
+
+//read the value from the  inputDescE1
+
+let textDesc=inputDescEl.value;
+
+//converting the value into the number
+let expense=parseInt(textAmount,10);
+
+//put it in object
+expenseItem.desc=textDesc;
+expenseItem.amount=expense;
+allExpenses.push(expenseItem);
+console.clear();
+console.table(allExpenses);
+
 
 totalExpense=totalExpense+expense;
 headingElement.textContent=totalExpense;
